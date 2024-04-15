822383e5f338e5382e184408c932a9918decb334
unknown <md.sabeel10@gmail.com>
2024-04-15 21:41:39 +0530

    commit message

--- pages/index.js
+++ pages/index.js
@@ -49,10 +49,10 @@ export default function App(props) {
     const userIndex = users.filter((user) => user.email === logindata.loginEmail);
   console.log(userIndex[0]);
   if(userIndex!=""){      
-      console.log("user has been found");
+      console.log("user has been founded");
   }
   else{
-    console.log("user has not been found");
+    console.log("user has not been founded");
   }
  }
   catch(error){
